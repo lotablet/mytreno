bc0adfc2b08eb62f422366f564cd540fe4025090
LoTableT <62808137+lotablet@users.noreply.github.com>
2025-05-10 20:00:52 +0200

    Update config_flow.py

--- custom_components/mytreno/config_flow.py
+++ custom_components/mytreno/config_flow.py
@@ -3001,7 +3001,7 @@ STATIONS = {
 }
 
 class MyTrenoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
+    VERSION = 2
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
